6da90e94a162efcb6b5441f817403c7be37bacab
isvargasm <ismael.vargas@storecheck.com>
2025-12-14 13:17:53 -0600

    eliminar service worker

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,19 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for (let registration of registrations) {
+      console.log('Matando Service Worker zombie:', registration);
+      registration.unregister();
+    }
+    // Opcional: Recargar la página automáticamente si se encontró uno
+    if (registrations.length > 0) {
+      window.location.reload();
+    }
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <App />
 )
